63164dd3bc57b2bce16226f2a15da5dca4e93db5
Deepali shukla <shukladeepali733@gmail.com>
2025-12-26 17:59:14 +0530

    Refactor Servers.java for improved readability

--- Servers.java
+++ Servers.java
@@ -35,7 +35,7 @@ public class Servers implements ActionListener {
         t.add(p1);
         p1.setLayout(null);
 
-        ImageIcon il = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        ImageIcon il = new ImageIcon(ClassLoader.getSystemResource("3.png"));
         Image i2 = il.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel back = new JLabel(i3);
@@ -48,28 +48,28 @@ public class Servers implements ActionListener {
 
             }
         });
-        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("1.png"));
         Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
         JLabel profile = new JLabel(i6);
         profile.setBounds(40, 10, 50, 50);
         p1.add(profile);
 
-        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("video.png"));
         Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
         JLabel video = new JLabel(i9);
         video.setBounds(300, 20, 30, 30);
         p1.add(video);
 
-        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("phone.png"));
         Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
         ImageIcon i12 = new ImageIcon(i11);
         JLabel phone = new JLabel(i12);
         phone.setBounds(360, 20, 35, 30);
         p1.add(phone);
 
-        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("3icon.png"));
         Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
         ImageIcon i15 = new ImageIcon(i14);
         JLabel morevert = new JLabel(i15);
@@ -276,5 +276,6 @@ public class Servers implements ActionListener {
 
 
     
+
 
 }
